3e324790d3514c4e90d1724b33235ac6ce6c3d9f
srinathredbery <srinath@redberylit.com>
2021-07-07 22:49:09 +0530

    ag grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+   const rowData = [
+       {make: "Toyota", model: "Celica", price: 35000},
+       {make: "Ford", model: "Mondeo", price: 32000},
+       {make: "Porsche", model: "Boxter", price: 72000}
+   ];
+
+   return (
+       <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+           <AgGridReact
+               rowData={rowData}>
+               <AgGridColumn field="make"></AgGridColumn>
+               <AgGridColumn field="model"></AgGridColumn>
+               <AgGridColumn field="price"></AgGridColumn>
+           </AgGridReact>
+       </div>
+   );
+};
+
 
 export default App;
